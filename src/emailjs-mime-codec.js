nifgraup/emailjs-mime-codec

commit 9be182ef6436fbaa05e389243d5e7ac3ddc577a0

Join quoted-printable mimewords before decoding
"cherry-picked" from https://github.com/andris9/mimelib/pull/32/commits/0ce93dcdc2b1020852e11f6d3fe8ac0226ec92f9

src/emailjs-mime-codec.js
@@ -330,7 +330,7 @@
          */
         mimeWordsDecode: function(str) {
             str = (str || "").toString().replace(/(=\?[^?]+\?[QqBb]\?[^?]+\?=)\s+(?==\?[^?]+\?[QqBb]\?[^?]*\?=)/g, "$1");
-            str = (str || "").toString().replace(/\?==\?[uU][tT][fF]-8\?[bB]\?/g, ""); // join bytes of multi-byte UTF-8
+            str = str.replace(/\?==\?[uU][tT][fF]-8\?[QqBb]\?/g, ""); // join bytes of multi-byte UTF-8
 
             str = str.replace(/\=\?[\w_\-\*]+\?[QqBb]\?[^\?]+\?\=/g, function(mimeWord) {
                 return mimecodec.mimeWordDecode(mimeWord);

test/mimecodec-unit.js
@@ -233,9 +233,13 @@ define(['chai', '../src/emailjs-mime-codec'], function(chai, mimecodec) {
                 expect(mimecodec.mimeWordsDecode(output2)).to.equal(input2);
             });
 
-            it('should join bytes of multi-byte UTF-8 characters before parsing', function() {
+            it('should join bytes of Base64 multi-byte UTF-8 characters before parsing', function() {
                 expect('GLG: Regulation of Taxi in China - 张一兵').to.equal(mimecodec.mimeWordsDecode("=?utf-8?B?R0xHOiBSZWd1bGF0aW9uIG9mIFRheGkgaW4gQ2hpbmEgLSDl?= =?utf-8?B?vKDkuIDlhbU=?="));
                 expect('***SPAM*** Очки виртуальной реальности').to.equal(mimecodec.mimeWordsDecode("***SPAM*** =?utf-8?B?0J7Rh9C60Lgg0LLQuNGA0YLRg9Cw0LvRjNC90L7QuSDR?=\r\n	=?utf-8?B?gNC10LDQu9GM0L3QvtGB0YLQuA==?="));
+            });
+
+            it('should join bytes of Quoted-Printable multi-byte UTF-8 characters before parsing', function() {
+                expect('гос (передай кому надо тоже').to.equal(mimecodec.mimeWordsDecode("=?utf-8?Q?=D0=B3=D0=BE=D1=81_?==?utf-8?Q?(=D0=BF=D0=B5=D1=80=D0=B5=D0=B4=D0=B0=D0=B9_=D0=BA=D0=BE=D0?= =?utf-8?Q?=BC=D1=83_=D0=BD=D0=B0=D0=B4=D0=BE_=D1=82=D0=BE=D0=B6=D0=B5?="));
             });
 
             it('should correclty parse this ISO-2022-JP encoded string', function() {

commit 93e15336cca77b29fbd6ccc269bcfe4e3da66334

mimeWordsDecode: Only join UTF-8 words with same encoding
regressed in 9be182ef6436fbaa05e389243d5e7ac3ddc577a0

src/emailjs-mime-codec.js
@@ -330,7 +330,13 @@
          */
         mimeWordsDecode: function(str) {
             str = (str || "").toString().replace(/(=\?[^?]+\?[QqBb]\?[^?]+\?=)\s+(?==\?[^?]+\?[QqBb]\?[^?]*\?=)/g, "$1");
-            str = str.replace(/\?==\?[uU][tT][fF]-8\?[QqBb]\?/g, ""); // join bytes of multi-byte UTF-8
+            // join bytes of multi-byte UTF-8
+            var prevEncoding;
+            str = str.replace(/(\?=)?=\?[uU][tT][fF]-8\?([QqBb])\?/g, function(match, endOfPrevWord, encoding) {
+                var result = (endOfPrevWord && encoding === prevEncoding) ? '' : match;
+                prevEncoding = encoding;
+                return result;
+            });
 
             str = str.replace(/\=\?[\w_\-\*]+\?[QqBb]\?[^\?]+\?\=/g, function(mimeWord) {
                 return mimecodec.mimeWordDecode(mimeWord.replace(/\s+/g, ''));

test/mimecodec-unit.js
@@ -242,6 +242,10 @@ define(['chai', '../src/emailjs-mime-codec'], function(chai, mimecodec) {
                 expect('гос (передай кому надо тоже').to.equal(mimecodec.mimeWordsDecode("=?utf-8?Q?=D0=B3=D0=BE=D1=81_?==?utf-8?Q?(=D0=BF=D0=B5=D1=80=D0=B5=D0=B4=D0=B0=D0=B9_=D0=BA=D0=BE=D0?= =?utf-8?Q?=BC=D1=83_=D0=BD=D0=B0=D0=B4=D0=BE_=D1=82=D0=BE=D0=B6=D0=B5?="));
             });
 
+            it('should not join together encoded words of different encoding', function() {
+                expect(' ⏯ Trendy Netflix Redesigns + ✈️ Airport App Challenge').to.equal(mimecodec.mimeWordsDecode('=?utf-8?b?IOKPryBUcmVuZHkgTmV0ZmxpeCBSZWRlc2lnbnMgKyDinIjvuI8g?= =?utf-8?q?Airport_App_Challenge?='));
+            });
+
             it('should decode Quoted-Printable with bad line split', function() {
                 expect('гос (передай кому надо тоже').to.equal(mimecodec.mimeWordsDecode("=?utf-8?Q?=D0=B3=D0=BE=D1=81_?==?utf-8?Q?(=D0=BF=D0=B5=D1=80=D0=B5= D0=B4=D0=B0=D0=B9_=D0=BA=D0=BE=D0?= =?utf-8?Q?=BC=D1=83_=D0=BD=D0=B0=D0=B4=D0=BE_=D1=82=D0=BE=D0=B6=D0=B5?="));
             });